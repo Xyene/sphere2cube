91d8003164735e8d770fdbcc7bab6991fe65b40b
Tudor <tbrindus@gmail.com>
2015-08-26 21:58:34 -0400

    reStructuredText because PYPI desires it

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages, version
 
-with open('README.md') as file:
+with open('README.rst') as file:
     long_description = file.read()
 
-setup(name='sphere2cube.py',
+setup(name='sphere2cube',
       version='0.1.0',
       description='Python script to map an equirectangular (cylindrical projection; skysphere) map into 6 cube (cubemap; skybox) faces',
       long_description=long_description,
@@ -25,6 +25,11 @@ setup(name='sphere2cube.py',
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
           'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
           'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
